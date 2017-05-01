f279e9d2d1bf91ba6bd2192090cbb2d226ff5afb
dshelters <dshelters@gmail.com>
2017-05-01 14:42:58 -0700

    (beginning) CommandLine Tic Tac Toe

---
+++ commandLineTicTacToe.js
@@ -0,0 +1,9 @@
+//
+
+const TTT = () => {
+  console.log('Welcome to TicTacToe!');
+  const player1 = window.prompt('Please enter the name for player 1.');
+  console.log(`Welcome ${player1}!`);
+}
+
+TTT();
